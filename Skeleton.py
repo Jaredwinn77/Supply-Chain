63aa3571d258821069c5e9cc70371f6e34c77832
Jaredwinn77 <134099303+Jaredwinn77@users.noreply.github.com>
2024-10-10 15:30:55 -0600

    Added edge list tracking to breadth first search

--- Skeleton.py
+++ Skeleton.py
@@ -158,7 +158,7 @@ def spectral_radius(weighted, subnetworks):
 
   return radii
 
-def breadth_first(G, starting_node):
+def breadth_first(G,starting_node):
     """This function is a breadth first approach to identifying subnetworks, it iterates through each subnetwork in working_subnetworks, adding a single level
     this could be more than a single edge/node pair if the edge is connected to more than one other node via edges of the same color. Once the stopping criteria are met the
     subnetwork is popped off and added to completed_subnetworks"""
@@ -175,64 +175,68 @@ def breadth_first(G, starting_node):
     # Initialize lists
     completed_subnetworks = []
     working_subnetworks = []
-
+    edge_lists = []
     # We add each element as a list
-    working_subnetworks.append([[starting_node], [starting_node]])
+    working_subnetworks.append([[starting_node],[starting_node],[]])
 
     visited = False
 
     # Loop through each subnetwork, adding one node to each network and popping of networks once they are finished
     while working_subnetworks:
-      # We work on each subnetwork individually
-      for subnetwork in working_subnetworks:
-        # This should pop the current subnetwork not the first one
-        index = working_subnetworks.index(subnetwork)
-        working_subnetworks.pop(index)
-        working_level = subnetwork[0][-1]
-        if isinstance(working_level, list):
-          k = len(working_level)
-        else:
-          k = 1
-        for i in range(k):
-          if isinstance(working_level, list):
-            node = working_level[i]
-          else:
-            node = working_level
-          # Collect all incoming edges
-          incoming_edges = list(G.in_edges(node, data=True))
-          # Creates a dictionary of all colors flowing into a node
-          color_groups = {}
-          for u, v, data in incoming_edges:
-            color = data.get('color')
-            if color not in color_groups:
-              color_groups[color] = []
-            color_groups[color].append(u)
-          # for each of the color groups we create a copy of the subnetwork and append all of the nodes in that group
-          for key in color_groups.keys():
-            # using deep copy ensures that the correct subnetworks are modified
-            new_subnetwork = copy.deepcopy(subnetwork)
-            for value in color_groups[key]:
-              # we need to check if the nodes are already in visited
-              if value in new_subnetwork[1]:
-                visited = True
-              else:
-                new_subnetwork[1].append(value)
-            if visited:
-              new_subnetwork[0].append(color_groups[key])
-              completed_subnetworks.append(new_subnetwork[0])
-              visited = False
+        # We work on each subnetwork individually
+        for subnetwork in working_subnetworks:
+            # This should pop the current subnetwork not the first one
+            index = working_subnetworks.index(subnetwork)
+            working_subnetworks.pop(index)
+            working_level = subnetwork[0][-1]
+            if isinstance(working_level,list):
+                k = len(working_level)
             else:
-              new_subnetwork[0].append(color_groups[key])
-              working_subnetworks.append(new_subnetwork)
+                k = 1
+            for i in range(k):
+                if isinstance(working_level, list):
+                    node = working_level[i]
+                else:
+                    node = working_level
+                # Collect all incoming edges
+                incoming_edges = list(G.in_edges(node,data = True))
+                # Creates a dictionary of all colors flowing into a node
+                color_groups = {}
+                for u, v, data in incoming_edges:
+                    color = data.get('color')
+                    if color not in color_groups:
+                        color_groups[color] = []
+                    color_groups[color].append(u)
+                # for each of the color groups we create a copy of the subnetwork and append all of the nodes in that group
+                for key in color_groups.keys():
+                    # using deep copy ensures that the correct subnetworks are modified
+                    new_subnetwork = copy.deepcopy(subnetwork)
+                    for value in color_groups[key]:
+                    # we need to check if the nodes are already in visited
+                        if value in new_subnetwork[1]:
+                            visited = True
+                        else:
+                            new_subnetwork[1].append(value)
+                    if visited:
+                        new_subnetwork[0].append(color_groups[key])
+                        # add an edge list
+                        new_subnetwork[-1].append((node,color_groups[key]))
+                        completed_subnetworks.append(new_subnetwork[0])
+                        edge_lists.append(new_subnetwork[-1])
+                        visited = False
+                    else:
+                        new_subnetwork[0].append(color_groups[key])
+                        new_subnetwork[-1].append((node, color_groups[key]))
+                        working_subnetworks.append(new_subnetwork)
     final = []
     for network in completed_subnetworks:
-      flat = []
-      stack = list(network)
-      while stack:
-        item = stack.pop()
-        if isinstance(item, list):
-          stack.extend(item)
-        else:
-          flat.append(item)
-      final.append(flat[::-1])
-    return final
+        flat = []
+        stack = list(network)
+        while stack:
+            item = stack.pop()
+            if isinstance(item,list):
+                stack.extend(item)
+            else:
+                flat.append(item)
+        final.append(flat[::-1])
+    return final, edge_lists
